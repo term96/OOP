3a0489f12efffc30edb43b09ee6be78611e257f3
term96 <term96@yandex.com>
2016-09-10 16:09:23 +0300

    Выполнены задания 1-3 ЛР 1
    Выполнены задания FindText, FlipByte, Invert

---
+++ FlipByte/Tests/test.bat
@@ -0,0 +1,21 @@
+@echo off
+
+echo Верный результат: 96, результат программы:
+flipbyte.exe 6
+pause
+cls
+
+echo Верный результат: 255, результат программы:
+flipbyte.exe 255
+pause
+cls
+
+echo Верный результат: 0, результат программы:
+flipbyte.exe 0
+pause
+cls
+
+echo Верный результат: 137, результат программы:
+flipbyte.exe 145
+pause
+cls
